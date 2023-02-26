fc1c598168a6602ff319b146cc425a1c770f28e4
Jeffery Patterson <jpatterson933@ucla.edu>
2023-02-25 16:35:28 -0800

    updated readme. Began refactoring code.

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-# library-search
+# Library of Congress - Basic Web Page Search
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-no-red.svg)](https://bitbucket.org/lbesson/ansi-colors)
+
+# Table of Contents
+1. [License MIT](#license-information)
+2. [Description](#application-description)
+3. [Installation](#installation-instructions)
+4. [Usage](#application-usage)
+5. [Languages Used](#languages-used)
+6. [Dependencies Used](#dependencies-used)
+7. [Contribution Guidelines](#contribution-guidelines)
+8. [Testing](#testing-instrutions)
+9. [Known Issues/Errors](#known-issues/errors)
+10. [Questions](#questions)
+
+# License Information
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+[MIT License](https://www.mit.edu/~amini/LICENSE.md) Documentation
+
+Please refer to license documentation for any questions regarding reusing 
+this software or any code within this application.
+
+[Back to Top](#table-of-contents)
+
+# Application Description
+
+Search the Library of Congress using Fetch method in Javascript and return a list of books .
+
+[Back to Top](#table-of-contents)
+
+# Installation Instructions
+
+NA
+
+[Back to Top](#table-of-contents)
+
+# Application Usage
+
+Purpose is to display a clean effective use for implementing a search feature on a website that allows ua USER  to find something.
+
+[Back to Top](#table-of-contents)
+
+# Languages Used
+
+html, css, javascript
+
+[Back to Top](#table-of-contents)
+
+# Dependencies Used
+
+na
+
+[Back to Top](#table-of-contents)
+
+# Contribution Guidelines
+
+NA
+
+[Back to Top](#table-of-contents)
+
+# Testing Instrutions
+
+NA
+
+[Back to Top](#table-of-contents)
+
+# Known Issues/Errors
+
+Yes. Code has multiple problems that are beginning to be worked out starting 2/25.
+
+[Back to Top](#table-of-contents)
+
+# Questions
+
+If you have any questions that have not been answered, please send me an email: designframebuildweb@gmail.com
+
+## Checkout My GitHub!
+
+[Back to Top](#table-of-contents)
+
+### [jpatterson933](https://github.com/jpatterson933)
+### [Live Application](https://github.com/jpatterson933/library-search)
+### [GitHub Repository](https://github.com/jpatterson933/library-search)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,69 @@
 
+// function to create query 
+const results = () => {
+  // grab html elements
+  var inputTest = $('#search').val().trim();
+  var formTest = $('#dropdown-item').val().trim();
+  let query = '&format=' + formTest + '&q=' + inputTest + "&fo=json";
+  let requestUrl = 'https://www.loc.gov/search/?' + query;
+  return requestUrl;
 
-  //on click event for our form button that calls our data
-  $("#button").on("click", function (event) {
-    //prevents page from reloading
-    event.preventDefault();
+}
 
-    //these two variables grab the value of our two input options and trim the white space
-    var inputTest = $('#search').val().trim();
-    var formTest = $('#dropdown-item').val().trim();
+//on click event for our form button that calls our data
+$("#button").on("click", function (event) {
+  // prevents page from reloading
+  event.preventDefault();
 
-    //this query variable combines our url from the two inputs above
-    let query = '&format=' + formTest + '&q=' + inputTest + "&fo=json";
+  //use fetch inside of your onclick event
+  fetch(results())
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var results = data.results;
+      console.log(results)
 
-    
-    
-    //this combines our base url with the url that was created from the two inputed values
-    let requestUrl = 'https://www.loc.gov/search/?' + query;
+      /*
 
-    console.log(requestUrl)
-
-    //use fetch inside of your onclick event
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        var results = data.results;
-
-        console.log(results)
-        //for loop that loops through the lenght of our results
-        for (var i = 0; i < results.length; i++){
-          
-          //consol log the date
-          console.log(results[i].title)
-          //create html element and assign it to variable
-          var infoDisplay = $("<h3 class='text'></h3>");
-          //append html element to our class .card in our html page
-          $(".info-display").append(infoDisplay);    
-          //appends innher html to newly created html element
-          $(infoDisplay).append("Title: " + results[i].title).innerhtml;
-         
-          
-          if (results[i].location) {
-            //console logs
-            console.log(results[i].location[0])
-            //creates new variables and this variable creates new html with class text 2
-            var infoDisplayTwo = $("<p class='text2'></p>");
-            //grabbing our class .text from infoDisplay above, we append our new variable underneath it
-            $(".text").append(infoDisplayTwo);
-            //Then we append our inner html to the html created in infoDisplayTwo variable
-            $(infoDisplayTwo).append("Location: " + results[i].location[0]).innerhtml
-            
-
-          }
-          
-          if (results[i].description){
-            //consol logs description 
-            console.log(results[i].description[0])
-            //creaes new html element and assigns it a variable
-            var infoDisplayDescription = $("<p class='text3' style='font-size: .5em;'></p>");
-            //appends new html element to class .text2 which is where our location data is
-            $(".text2").append(infoDisplayDescription);
-            //changes inner html of newly created html element
-            $(infoDisplayDescription).append("Description: " + results[i].description[0]).innerhtml;
-
-          }
-        }
-
-
-      });
       
-  })
+      */
+
+      //for loop that loops through the lenght of our results
+      for (var i = 0; i < results.length; i++) {
+
+        //create html element and assign it to variable
+        var infoDisplay = $("<h3 class='text'></h3>");
+        //append html element to our class .card in our html page
+        $(".info-display").append(infoDisplay);
+        //appends innher html to newly created html element
+        $(infoDisplay).append("Title: " + results[i].title).innerhtml;
+
+
+        if (results[i].location) {
+          //console logs
+          console.log(results[i].location[0])
+          //creates new variables and this variable creates new html with class text 2
+          var infoDisplayTwo = $("<p class='text2'></p>");
+          //grabbing our class .text from infoDisplay above, we append our new variable underneath it
+          $(".text").append(infoDisplayTwo);
+          //Then we append our inner html to the html created in infoDisplayTwo variable
+          $(infoDisplayTwo).append("Location: " + results[i].location[0]).innerhtml
+        }
+        // here we are saying if there is a description for the results
+        if (results[i].description) {
+          //creaes new html element and assigns it a variable
+          var infoDisplayDescription = $("<p class='text3' style='font-size: .5em;'></p>");
+          //appends new html element to class .text2 which is where our location data is
+          $(".text2").append(infoDisplayDescription);
+          //changes inner html of newly created html element
+          $(infoDisplayDescription).append("Description: " + results[i].description[0]).innerhtml;
+
+        }
+      }
+
+
+    });
+
+})
 
